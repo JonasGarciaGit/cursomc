091a13a43394f0e7b79ab2e6d4a7767222e32813
kloppos120 <jonasemprego1@gmail.com>
2019-08-16 15:03:17 -0300

    Ajuste no endpoint /categorias/{id}

--- src/main/java/com/jonasgarcia/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/jonasgarcia/cursomc/resources/CategoriaResource.java
@@ -1,4 +1,5 @@
 package com.jonasgarcia.cursomc.resources;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,13 +16,11 @@ public class CategoriaResource {
 
 	@Autowired
 	private CategoriaService service;
-	
-	@RequestMapping(value = "/{id}" ,method =RequestMethod.GET)
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
-		
+
 		Categoria obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
-		
 	}
-	
 }
